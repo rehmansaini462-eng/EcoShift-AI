4a0628324fad28e907f7e365af88a3ea55f48dde
Rehman Saini <rehmansaini462@gmail.com>
2026-08-15 13:53:41 +0530

    Untrack .next directory and update .gitignore

--- .next/server/app/api/vehicle-spec/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/vehicle-spec/route.js")
-R.c("server/chunks/[externals]__0l8ei7u._.js")
-R.c("server/chunks/[root-of-the-server]__0ue_vx_._.js")
-R.c("server/chunks/_next-internal_server_app_api_vehicle-spec_route_actions_0h0tisw.js")
-R.m(39636)
-module.exports=R.m(39636).exports
--- .next/server/chunks/_next-internal_server_app_api_vehicle-spec_route_actions_0h0tisw.js
+++
@@ -1,3 +0,0 @@
-module.exports=[63144,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_vehicle-spec_route_actions_0h0tisw.js.map
